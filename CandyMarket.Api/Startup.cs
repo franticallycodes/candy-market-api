82661b00377359249ec1c658c45cc29a71d912e6
Matt Gill <mtgill90@outlook.com>
2019-11-06 16:47:17 -0600

    wip - app.UseMVC throwing an exception

--- CandyMarket.Api/Startup.cs
+++ CandyMarket.Api/Startup.cs
@@ -28,6 +28,12 @@ namespace CandyMarket.Api
         {
             services.AddControllers();
             services.AddScoped<ICandyRepository, CandyRepository>();
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,16 +44,20 @@ namespace CandyMarket.Api
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+          //  app.UseHttpsRedirection();
 
             app.UseRouting();
 
-            app.UseAuthorization();
+           // app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
+            app.UseCors("MyPolicy");
+
+           app.UseMvc();
         }
     }
 }
